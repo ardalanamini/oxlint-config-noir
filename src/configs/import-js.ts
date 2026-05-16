48fd8a74e1cec7082ca55992563386f2e617600b
Ardalan Amini <ardalanamini22@gmail.com>
2026-05-16 15:52:28 +0300

    Initial commit
    Signed-off-by: Ardalan Amini <ardalanamini22@gmail.com>

---
+++ src/configs/import-js.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "oxlint";
+import { base } from "./base.ts";
+
+export const importJS = defineConfig({
+  extends  : [base],
+  jsPlugins: [
+    {
+      name     : "import-js",
+      specifier: "eslint-plugin-import",
+    },
+  ],
+  rules: {
+    "import-js/order": [
+      "error",
+      {
+        groups     : ["type", "builtin", "external", "internal", "unknown", "sibling", "index", "parent", "object"],
+        alphabetize: {
+          order          : "asc",
+          orderImportKind: "asc",
+          caseInsensitive: false,
+        },
+        named: {
+          enabled: true,
+          types  : "types-first",
+        },
+      },
+    ],
+  },
+});
